cd9d80d03912d21869006862d7e806cb2f5d283e
rizkyakhid <rizkyakhid@gmail.com>
2020-11-23 18:03:18 +0700

    fixes and styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
 import React from 'react'
-import axios from 'axios'
-import CharacterList from './CharacterList'
+import Home from './views/Home'
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      characters: []
-    }
-  }
-
-  componentDidMount() {
-    axios({
-      method: 'get',
-      url: 'https://finalspaceapi.com/api/v0/character'
-    })
-      .then(({ data }) => {
-        this.setState({ characters: data })
-      })
-  }
-
-  render() {
-    const { characters } = this.state
-    return (
-      <React.Fragment>
-        <div className="container my-3">
-          <h1>Character List</h1>
-          <CharacterList characters={characters} />
-        </div>
-      </React.Fragment>
-    )
-  }
+function App() {
+  return (
+    <Home></Home>
+  );
 }
 
-export default App
+export default App;
